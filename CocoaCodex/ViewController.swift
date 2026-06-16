cbf9d8d433af355a941d3ad8e55ef41871614f28
ziezheng <ziezheng@tencent.com>
2026-06-16 15:59:05 +0800

    Initial Commit

---
+++ CocoaCodex/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CocoaCodex
+//
+//  Created by zie on 2026/6/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
